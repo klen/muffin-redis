5db1f91cd9017c837388b9b37c885dbf28c7f224
Kirill Klenov <horneds@gmail.com>
2023-02-21 02:47:00 +0700

    feat: support muffin 0.88

--- muffin_redis/__init__.py
+++ muffin_redis/__init__.py
@@ -1,11 +1,14 @@
 """Redis support for Muffin framework."""
 
-import typing as t
+from contextlib import suppress
+from typing import Optional
 
-from aioredis import BlockingConnectionPool, ConnectionPool, Redis, RedisError
 from asgi_tools._compat import json_dumps, json_loads
-
+from muffin import Application
 from muffin.plugins import BasePlugin
+
+from redis.asyncio import BlockingConnectionPool, ConnectionPool, Redis, RedisError
+from redis.typing import EncodableT, KeyT
 
 __version__ = "3.2.0"
 
@@ -14,22 +17,51 @@ class Plugin(BasePlugin):
 
     """Manage Redis."""
 
-    name = 'redis'
+    name = "redis"
     defaults = {
-        'url': 'redis://localhost',
-        'db': None,
-        'password': None,
-        'encoding': 'utf-8',
-        'decode_responses': True,
-        'jsonify': False,
-
-        'poolsize': 10,
-        'blocking': True,
-        'timeout': 20,
+        "url": "redis://localhost",
+        "db": None,
+        "password": None,
+        "encoding": "utf-8",
+        "decode_responses": True,
+        "jsonify": False,
+        "poolsize": 10,
+        "blocking": True,
+        "timeout": 20,
     }
 
-    client = None
     Error = RedisError
+
+    def __init__(self, app: Optional[Application], **options):
+        """Initialize the plugin."""
+        self.__client__: Optional[Redis] = None
+        super().__init__(app, **options)
+
+    async def startup(self):
+        """Setup a redis connection."""
+        params = {
+            "db": self.cfg.db,
+            "password": self.cfg.password,
+            "decode_responses": self.cfg.decode_responses,
+            "encoding": self.cfg.encoding,
+        }
+
+        if self.cfg.blocking:
+            params["timeout"] = self.cfg.timeout
+
+        pool_cls = BlockingConnectionPool if self.cfg.blocking else ConnectionPool
+        pool = pool_cls.from_url(
+            self.cfg.url, max_connections=self.cfg.poolsize, **params
+        )
+        self.__client__ = Redis(connection_pool=pool)
+
+    @property
+    def client(self) -> Redis:
+        """Return a client instance."""
+        if self.__client__ is None:
+            raise RuntimeError("Redis Plugin should be started")
+
+        return self.__client__
 
     def __getattr__(self, name):
         """Proxy attribute to self connection."""
@@ -42,50 +74,35 @@ class Plugin(BasePlugin):
     async def __aexit__(self, *args):
         await self.client.__aexit__(*args)
 
-    async def startup(self):
-        """Setup a redis connection."""
-        params = {
-            'db': self.cfg.db, 'password': self.cfg.password,
-            'decode_responses': self.cfg.decode_responses, 'encoding': self.cfg.encoding}
-
-        if self.cfg.blocking:
-            params['timeout'] = self.cfg.timeout
-
-        pool_cls = BlockingConnectionPool if self.cfg.blocking else ConnectionPool
-        pool = pool_cls.from_url(self.cfg.url, max_connections=self.cfg.poolsize, **params)
-        self.client = Redis(connection_pool=pool)
-
     async def shutdown(self):
         """Close the redis pool."""
         await self.client.close()
         await self.client.connection_pool.disconnect()
 
-    def set(self, key, value, *, jsonify: bool = None, **options) -> t.Awaitable:
+    def set(
+        self,
+        name: KeyT,
+        value: EncodableT,
+        *,
+        jsonify: Optional[bool] = None,
+        **options
+    ):
         """Store the given value into Redis."""
-        if (self.cfg.jsonify if jsonify is None else jsonify):
+        if self.cfg.jsonify if jsonify is None else jsonify:
             value = json_dumps(value)
 
-        client = self.client
-        if client is None:
-            raise RuntimeError('Redis Plugin should be started')
+        return self.client.set(name, value, **options)
 
-        return client.set(key, value, **options)
-
-    async def get(self, key, *, jsonify: bool = None, **options):
+    async def get(self, key, *, jsonify: Optional[bool] = None, **options):
         """Decode the value."""
         client = self.client
-        if client is None:
-            raise RuntimeError('Redis Plugin should be started')
-
         value = await client.get(key, **options)
 
         if value is not None and (self.cfg.jsonify if jsonify is None else jsonify):
             if isinstance(value, bytes):
-                value = value.decode('utf-8')
+                value = value.decode("utf-8")
 
-            try:
+            with suppress(ValueError):
                 return json_loads(value)
-            except ValueError:
-                pass
 
         return value
--- tests.py
+++ tests.py
@@ -1,41 +1,43 @@
 import asyncio
+
 import muffin
 import pytest
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def aiolib():
     """Disable uvloop for tests."""
-    return ('asyncio', {'use_uvloop': False})
+    return ("asyncio", {"use_uvloop": False})
 
 
 @pytest.fixture
 async def app(redis_url):
-    from muffin_redis import Plugin as Redis
+    from muffin_redis import Plugin
 
     app = muffin.Application(debug=True)
-    redis = Redis(app, url=redis_url)
+    redis = Plugin(app, url=redis_url)
     async with app.lifespan:
         yield app
         await redis.flushall()
 
 
 async def test_muffin_redis(app):
-    redis = app.plugins['redis']
+    redis = app.plugins["redis"]
 
-    result = await redis.get('key')
+    result = await redis.get("key")
     assert result is None
 
-    await redis.set('key', 'value', ex=10)
-    result = await redis.get('key')
-    assert result == 'value'
+    await redis.set("key", "value", ex=10)
+    result = await redis.get("key")
+    assert result == "value"
 
     import time
-    await redis.set('dict', {'now': time.time()}, jsonify=True)
-    result = await redis.get('dict', jsonify=True)
-    assert result and 'now' in result
 
-    result = await redis.get('unknown')
+    await redis.set("dict", {"now": time.time()}, jsonify=True)
+    result = await redis.get("dict", jsonify=True)
+    assert result and "now" in result
+
+    result = await redis.get("unknown")
     assert result is None
 
 
@@ -66,45 +68,44 @@ async def test_pool(redis_url):
 
 
 async def test_jsonnify(app):
-    redis = app.plugins['redis']
+    redis = app.plugins["redis"]
 
-    res = await redis.get('l1', jsonify=True)
+    res = await redis.get("l1", jsonify=True)
     assert res is None
 
-    await redis.set('l1', "[1, 2, 3")
-    res = await redis.get('l1', jsonify=True)
-    assert res == '[1, 2, 3'
+    await redis.set("l1", "[1, 2, 3")
+    res = await redis.get("l1", jsonify=True)
+    assert res == "[1, 2, 3"
 
-    await redis.set('l1', [1, 2, 3], jsonify=True)
-    res = await redis.get('l1', jsonify=True)
+    await redis.set("l1", [1, 2, 3], jsonify=True)
+    res = await redis.get("l1", jsonify=True)
     assert res == [1, 2, 3]
 
 
 async def test_simple_lock(app):
-    redis = app.plugins['redis']
+    redis = app.plugins["redis"]
 
-    async with redis.lock('l1') as lock:
+    async with redis.lock("l1") as lock:
         assert lock
 
-        async with redis.lock('l2') as lock2:
+        async with redis.lock("l2") as lock2:
             assert lock2
 
-    async with redis.lock('l1') as lock:
+    async with redis.lock("l1") as lock:
         assert lock
 
 
 async def test_muffin_redis_pubsub(app):
-    from asgi_tools._compat import aio_spawn, aio_sleep
+    from asgi_tools._compat import aio_sleep, aio_spawn
 
-    redis = app.plugins['redis']
+    redis = app.plugins["redis"]
 
     async def reader(channel_name: str):
-
         async def callback(channel):
             while True:
                 msg = await channel.get_message(ignore_subscribe_messages=True)
                 if msg:
-                    return msg['data']
+                    return msg["data"]
 
         async with redis.pubsub() as psub:
             await psub.subscribe(channel_name)
@@ -113,9 +114,9 @@ async def test_muffin_redis_pubsub(app):
 
         return res
 
-    async with aio_spawn(reader, 'chan:1') as task:
+    async with aio_spawn(reader, "chan:1") as task:
         await aio_sleep(0.1)
-        await redis.publish('chan:1', 'test')
+        await redis.publish("chan:1", "test")
         await aio_sleep(0.1)
 
-    assert task.result() == 'test'
+    assert task.result() == "test"
